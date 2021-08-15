3052268538c72950bbb653d9798eb26b406ec4c2
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-15 22:55:28 +0200

    Making the components functional

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-		
-  
-	render() {
+const UserItem = props => {
+	const { login, avatar_url, html_url } = props.user;
+	//komt van app.js, die het uit users haalt?
 
-    const {login, avatar_url, html_url } =this.props.user
-      //komt van app.js, die het uit users haalt?
-		return (
-			<div className='card text-center'>
-				<img src={avatar_url} alt='' className="round-img" style={{width: '60px'}}/>
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-        </div>
+	return (
+		<div className='card text-center'>
+			<img
+				src={avatar_url}
+				alt=''
+				className='round-img'
+				style={{ width: '60px' }}
+			/>
+			<h3>{login}</h3>
+			<div>
+				<a href={html_url} className='btn btn-dark btn-sm my-1'>
+					More
+				</a>
 			</div>
-		);
-	}
+		</div>
+	);
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
 }
 
 export default UserItem;
