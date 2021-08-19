aea4bcbc85ec7a420fe6cd0f6cc0be36b4cdf8e3
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-19 22:59:39 +0200

    alert to fill in something

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -9,7 +10,8 @@ class App extends Component {
 
 	state = {
 		users: [],
-		loading: false
+		loading: false,
+		alert: null
 	}
 
 
@@ -24,6 +26,12 @@ class App extends Component {
 	}
 	// Clear users from state
 	clearUsers = () => this.setState({ users: [], loading: false})
+	// Set Alert
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type } });
+
+		setTimeout(() => this.setState({ alert: null}), 5000);
+	}
 
 	render() {
 		
@@ -31,7 +39,8 @@ class App extends Component {
 			<div className='App'>
 				<Navbar title='Github Finder' icon='fab fa-github' />
 				<div className='container'>
-				<Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false}/>
+				<Alert alert={this.state.alert}/>
+				<Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false} setAlert={this.setAlert}/>
 					<Users loading={this.state.loading} users={this.state.users}/>
 				</div>
 			</div>
